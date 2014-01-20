af0ee85600627cc03adf40c8deedb8db4a932084
arvindayal <arvinddayal@gmail.com>
2014-01-20 11:44:48 -0700

    initial prompts

---
+++ js5.js
@@ -0,0 +1,20 @@
+var names = []
+var phone = []
+var street = []
+
+
+var start = prompt("How many disaster victims do you wish to enter?");
+
+var getNames = prompt("Enter victims name:");
+	names.push(getNames);
+
+var getPhone = prompt("Enter victims phone number:");
+	phone.push(getPhone);
+
+var getStreet = prompt("Enter victims street");
+	street.push(getStreet);
+
+
+console.log(names);
+console.log(phone);
+console.log(street);
