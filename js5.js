40a604609358a5f941b0aad236153f9d6f425ef9
arvindayal <arvinddayal@gmail.com>
2014-01-20 14:38:40 -0700

    disaster victims working

--- js5.js
+++ js5.js
@@ -3,18 +3,24 @@ var phone = []
 var street = []
 
 
-var start = prompt("How many disaster victims do you wish to enter?");
 
-var getNames = prompt("Enter victims name:");
-	names.push(getNames);
+var starter = parseInt(prompt("How many disaster victims do you wish to enter?","0"));
+	for (i = 0; i < starter; i++) {
 
-var getPhone = prompt("Enter victims phone number:");
-	phone.push(getPhone);
+	var getNames = prompt("Enter victims name:");
+		names.push(getNames);
 
-var getStreet = prompt("Enter victims street");
-	street.push(getStreet);
+	var getPhone = prompt("Enter victims phone number:");
+		phone.push(getPhone);
 
+	var getStreet = prompt("Enter victims street");
+		street.push(getStreet);
+};
 
 console.log(names);
 console.log(phone);
 console.log(street);
+
+
+
+// for (var i = 0; i < starter.length; i = starter.length)
